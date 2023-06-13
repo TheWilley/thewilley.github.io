4ee2614e563a9741caf30190a71032a09b21c5b5
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-06-13 08:59:55 +0200

    feat: made mobile view more user friendly

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,12 +5,12 @@ import { Link } from 'react-router-dom'
 function Sidebar() {
     function SidebarItem(props: {link_to: string, icon: IconDefinition, text: string}) {
         return (
-            <Link to={props.link_to}> <li className='text-gray-400 p-1 pl-0 m-1 rounded-lg text-xl hover:bg-gray-700 hover:text-white transition'> <span className='inline-flex'> <FontAwesomeIcon icon={props.icon} className='ml-2' /> <span className='pl-2 hidden group-hover:block'>{props.text}</span></span> </li></Link>
+            <Link to={props.link_to}> <li className='text-gray-400 p-1 pl-0 m-1 rounded-lg text-xl hover:bg-gray-700 hover:text-white transition'> <span className='inline-flex'> <FontAwesomeIcon icon={props.icon} className='ml-2' /> <span className='pl-2 hidden sm:group-hover:block'>{props.text}</span></span> </li></Link>
         )
     }
 
     return (
-        <nav className='h-screen bg-neutral-950 text-gray-500 fixed group w-16 ease-out transition-all duration-150 hover:w-48 overflow-hidden z-10'>
+        <nav className='h-screen bg-neutral-950 text-gray-500 fixed group w-16 ease-out transition-all duration-150 hover:w-16 sm:hover:w-48 overflow-hidden z-10'>
             <ul className='p-1 ml-1'>
                 <SidebarItem link_to='/' icon={faHouse} text='Home' />
                 <SidebarItem link_to='/blog' icon={faBlog} text='Blog' />
