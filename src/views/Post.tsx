7d166ccb93a0daaeaf31debcdef46f7fd808ca27
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-06-19 20:42:25 +0200

    style: added global css to make sure background changes accross all pages when switching dark and light mode

--- src/views/Post.tsx
+++ src/views/Post.tsx
@@ -41,7 +41,7 @@ function Post() {
                 )
             } else {
                 return (
-                    <div className='max-w-screen-2xl p-3 dark:!bg-neutral-950'>
+                    <div className='max-w-screen-2xl p-3'>
                         <img src={`${configuration.endpoint_url}${post?.data.attributes.thumbnail.data.attributes.formats.medium.url}`} className='h-96 max-h-64 w-full object-cover border rounded' loading="lazy" />
                         <div>
                             <h1 className="lg:text-5xl md:text-3xl sm:text-2xl text-xl font-bold mb-2 text-blue-500"> {post?.data.attributes.title} </h1>
