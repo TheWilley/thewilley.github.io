08e0e40760ebc3c9eaa01a47702e350ba1bf40bb
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-07-03 00:51:02 +0200

    style: fixed bug where copyright notice was not visible

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <footer className="p-3 text-center opacity-70 absolute bottom-0 ml-auto mr-auto text-center left-0 right-0">
+        <footer className="p-3 text-center opacity-70 absolute bottom-0 ml-auto mr-auto text-center left-0 right-0 dark:text-white">
             Copyright © 2023 TheWilley
         </footer>
     )
