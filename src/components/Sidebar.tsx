ebe142e0f4d15cbf1918bb47f4c2b87eb6e4131e
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-07-07 10:13:43 +0200

    style: quality of life changes

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 import { faBars, faClock, faHammer, faHome, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { useState } from 'react'
 
 function Sidebar() {
@@ -36,7 +36,7 @@ function Sidebar() {
     const renderCollapsedSidebar = () => {
         return (
             <>
-                <FontAwesomeIcon icon={faBars} className='absolute right-8 top-7 text-2xl cursor-pointer hover:opacity-70 sm:hidden block' onClick={() => setSidebarActive(true)} />
+                <FontAwesomeIcon icon={faBars} className='absolute right-8 top-6 text-2xl cursor-pointer hover:opacity-70 sm:hidden block' onClick={() => setSidebarActive(true)} />
                 <div className={`fixed w-screen h-screen z-10 bg-black opacity-40 sm:hidden block ${!sidebarActive && 'hidden'}`} onClick={() => setSidebarActive(false)} />
                 <div className={`fixed right-0 bg-blue-200  dark:bg-neutral-900 h-screen z-10 text-center text-2xl border-left block sm:hidden transition-all overflow-hidden ease-in-out duration-300 ${sidebarActive ? 'w-32' : 'w-0'}`}>
                     <ul className='[&>*]:p-2'>
@@ -53,7 +53,7 @@ function Sidebar() {
             <>
                 {renderCollapsedSidebar()}
                 <div className='text-left sm:text-center p-3 sm:text-5xl md:text-7xl text-3xl'>
-                    <h1 className='mt-2 font-mono'>TheWilley</h1>
+                    <Link to='/' className='mt-2 font-mono hover:text-blue-500 transition'>TheWilley</Link>
                 </div>
                 <div className='sm:flex justify-center hidden'>
                     <div className='rounded w-fit text-xl md:text-3xl border border-4 overflow-hidden p-1'>
